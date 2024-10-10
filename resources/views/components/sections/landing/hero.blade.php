f94ad3aad1ab7a0436c5141e3c1b0ae6c5d4d375
mathias <mathias.suchail@yahoo.com>
2024-10-10 05:13:54 +0200

    seo

--- resources/views/components/sections/landing/hero.blade.php
+++ resources/views/components/sections/landing/hero.blade.php
@@ -3,7 +3,7 @@
     id="profile"
     class="bg-gradient-to-br from-black to-indigo-950 min-h-screen flex items-center justify-center p-4 md:pt-52">
         <div class="flex flex-col gap-12 h-full items-center justify-center md:w-1/2">
-            <h1 class="text-white text-2xl md:text-3xl font-extrabold">Développeur web freelance - PHP Laravel</h1>
+            <h1 class="text-white text-2xl md:text-3xl font-extrabold text-center">Développeur web freelance - PHP Laravel</h1>
             <a href="#about">
                 <img id="hero-image" tooltip="sdfsqdf" class="w-72 rounded-xl shadow-3xl transition-all hover:scale-105 delay-200" src="{{ asset('images/mathias.jpg') }}" alt="Picture of Mathias, the Laravel Developer">
             </a>
